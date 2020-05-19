26916721ec1d7e4c9664d98f3621fc65815e8921
Sylvester Elorm Coch <sylvester.e.coch.22@dartmouth.edu>
2020-05-19 12:06:38 -0500

    changed browser logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from './layouts/Navbar.js'
 import HomeScreen from './components/HomeScreen'
 
-function App() {
-  return (
-    <div className="Container">
-      <Navbar/>
-      <HomeScreen/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="Container">
+        <Navbar/>
+        <HomeScreen/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
